38d298385afe00e149c9d010bc8b8f3ad0d4cf02
Shuai-xv <xv-shuai@foxmail.com>
2020-01-10 15:49:11 +0800

    commit file test.c

---
+++ test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    
+    bool a=1;
+    int b=2;
+    int c=0x23;
+    printf("hello,world\n");
+    printf("%d\t%d\n",b,c);
+    printf("%d",b&c);
+    return a;
+}
